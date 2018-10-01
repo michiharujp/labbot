98776c9bc07279d15d253ef5b93a8184fb7dca9f
michiharujp <michiharujp@icloud.com>
2018-10-01 21:13:25 +0900

    インデント修正

--- plugins/dictionary.py
+++ plugins/dictionary.py
@@ -6,18 +6,18 @@ from slackbot.bot import respond_to
 
 def make_soup_from(url):
     re = requests.get(url)
-        html = re.text
-        soup = BeautifulSoup(html, "html.parser")
-        return soup
+    html = re.text
+    soup = BeautifulSoup(html, "html.parser")
+    return soup
 
 def translate_from_weblio(word):
     url = "https://ejje.weblio.jp/content/{}".format(word)
-        soup = make_soup_from(url)
-        explain = soup.find("td", {"class":"content-explanation ej"})
-        if explain == None:
-            return None
-        else:
-            return explain.string
+    soup = make_soup_from(url)
+    explain = soup.find("td", {"class":"content-explanation ej"})
+    if explain == None:
+        return None
+    else:
+        return explain.string
 
 @respond_to("dict +(.+)")
 def respond_word(message, word):
