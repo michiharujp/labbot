817cd449538cb5cd17273a0f6fea90a58973833d
mosm <airman515@gmail.com>
2018-10-01 22:33:09 +0900

    サイコロに面の数を指定できるようにする

--- plugins/dice.py
+++ plugins/dice.py
@@ -3,7 +3,17 @@
 from slackbot.bot import respond_to
 import random
 
-@respond_to('dice')
-def respond(message):
-    roll = random.randint(1, 6)
+@respond_to(r'dice *([0-9]+)?')
+def respond(message, n):
+    """dice [面の数]"""
+
+    if n is None:
+        n = 6
+    elif int(n) < 2:
+        message.reply('サイコロは2面以上にしないと意味がないワン！')
+        return
+    else:
+        n = int(n)
+
+    roll = random.randint(1, n)
     message.reply(':game_die: {}'.format(roll))
