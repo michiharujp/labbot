0a44852936229f9c5c328e3212937d418b424c08
mosm <airman515@gmail.com>
2018-10-01 22:26:57 +0900

    コマンドの使い方を表示できるようにする

--- run.py
+++ run.py
@@ -1,6 +1,19 @@
 # coding: utf-8
 
-from slackbot.bot import Bot
+import six
+from slackbot.bot import Bot, default_reply
+
+@default_reply
+def custom_default_handler(message):
+    reply = [
+        u'"{}"は知らないコマンドだワン！'
+        u'使えるコマンドを見せるワン！\n'.format(message.body['text']),
+    ]
+    reply += [
+        u'    • `{}`'.format(v.__doc__ or p.pattern) 
+        for p, v in six.iteritems(message._plugins.commands['respond_to'])
+    ]
+    message.reply(u'\n'.join(reply))
 
 def main():
     bot = Bot()
