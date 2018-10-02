920fe824fb1cbad4d56f8343f4be6ea8b124967a
michiharujp <michiharujp@icloud.com>
2018-10-02 14:10:57 +0900

    shuffle関数とshuffle-members関数に分離

---
+++ plugins/shuffle.py
@@ -0,0 +1,20 @@
+# coding: utf-8
+
+import random
+from slackbot.bot import respond_to
+
+def rank_text(items):
+    msg = ''
+    for i, item in enumerate(items, start=1):
+        msg += "{}: {}\n".format(i,item)
+    return msg
+
+@respond_to(r'shuffle +(.*)')
+def shuffle(message, items):
+    """shuflle 要素1 要素2 要素3"""
+    items = items.split()
+    if len(items) < 2:
+        message.send('要素を２つ以上にして、空白区切りで渡すワン！')
+    else:
+        random.shuffle(items)
+        message.send(rank_text(items))
